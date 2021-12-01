d76971ef6459763e22e47a8e470e381d2815c132
azzy9 <19675382+azzy9@users.noreply.github.com>
2021-12-01 13:14:28 +0000

    Update generate.php
    Add functionality to extract addon.xml from latest plugin versions

--- generate.php
+++ generate.php
@@ -14,6 +14,17 @@ if( isset( $password ) && $password ) {
 	}
 }
 
+//require functions file for compatibility
+require 'includes/functions/functions.compatibility.php';
+
+//require class to help with scanning for addon xml's
+require 'includes/classes/helper/class.files.php';
+
+
+if( isset( $extract_files ) && $extract_files ) {
+	xml_extract( $plugin_dir, $show_info );
+}
+
 ouput_generate( $output_file, $plugin_dir, $show_info );
 
 if( isset( $hash_file ) && $hash_file ) {
@@ -21,9 +32,6 @@ if( isset( $hash_file ) && $hash_file ) {
 }
 
 function ouput_generate( $output_file, $plugin_dir, $show_info ) {
-
-	//require class to help with scanning for addon xml's
-	require 'includes/classes/helper/class.files.php';
 
 	//set the dir
 	$files = new files( $plugin_dir );
@@ -50,7 +58,7 @@ function ouput_generate( $output_file, $plugin_dir, $show_info ) {
 
 		$plugins_stats[ 'plugins' ]++;
 
-		if( file_exists ( $plugin . '/addon.xml' ) ) {
+		if( file_exists( $plugin . '/addon.xml' ) ) {
 
 			$plugins_stats[ 'included' ]++;
 
@@ -92,5 +100,109 @@ function output_hash( $output_file, $show_info ) {
 		echo 'Hash File Generated<br />';
 	}
 
+}
+
+//method to run through all folders then extracts latest addon.xml
+function xml_extract( $plugin_dir, $show_info ) {
+
+	//set the dir
+	$files = new files( $plugin_dir );
+
+	$files->options['recursive']	= false;
+	$files->options['list_dir']		= true;
+	$files->options['list_files']	= false;
+
+	$plugin_folders = $files->scan();
+
+	//loop through all plugin folders and scan for zip files
+	foreach( $plugin_folders as $plugin ) {
+
+		//set the dir
+		$zips = new files( $plugin );
+
+		$zips->options['recursive']		= false;
+		$zips->options['list_dir']		= true;
+		$zips->options['list_files']	= true;
+		$zips->options['file_types']	= array( 'zip' );
+
+		//get list of zip files
+		$zip_files = $zips->scan();
+
+		//get latest version in folder
+		$latest_version = latest_version( $zip_files );
+
+		if( $latest_version ) {
+			//get latest version of zip
+			$latest_zip = array_search_contains( $latest_version, $zip_files );
+
+			$zip = new ZipArchive;
+			$res = $zip->open($zip_files[ $latest_zip ]);
+			if ($res === TRUE) {
+				if($addonxml = $zip->getFromName( str_replace( $plugin_dir . '/', '', $plugin ) . '/addon.xml')) {
+					file_put_contents( $plugin . '/addon.xml' , $addonxml);
+					if( $show_info ) {
+						echo 'Updating: ', $plugin, '/addon.xml<br />';
+					}
+				} else {
+					if( $show_info ) {
+						echo 'Error updating: ', $plugin, '/addon.xml<br />';
+					}
+				}
+
+				$zip->close();
+
+			} else {
+				if( $show_info ) {
+					echo 'Zip Error: ', $zip_files[ $latest_zip ], '<br />';
+				}
+			}
+
+		}
+	}
+
+}
+
+//method to get latest zip file
+function latest_version( $zip_files ) {
+
+	//turn into array of versions
+	$versions = array_map(
+		function( $val ){
+			$parts = explode('-', $val);
+			if( $parts ) {
+				return str_replace( '.zip', '', end( $parts ) );
+			}
+			return array();
+		},
+		$zip_files
+	);
+
+	//sort versions
+	if( $versions ) {
+		usort( $versions, function($a, $b) {
+			if ($a == $b) {
+				return 0;
+			}
+			return version_compare($a, $b, '<') ? 1 : -1;
+		});
+	}
+
+	//return largest version
+	return ( isset( $versions[0] ) ? $versions[0] : false );
+
+}
+
+//function works the same as array_search except is contains instead of a full match
+function array_search_contains( $needle, $haystack_array ) {
+
+	if( $needle && $haystack_array ) {
+		foreach( $haystack_array as $haystack_id => $haystack ) {
+			if( str_contains( $haystack, $needle ) ) {
+				return $haystack_id;
+			}
+		}
+	}
+
+	return false;
 }
 
