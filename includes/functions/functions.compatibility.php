b952728b4db84ee92467b9e5f07491bc94ec6592
azzy9 <19675382+azzy9@users.noreply.github.com>
2021-12-01 13:11:22 +0000

    Create functions.compatibility.php
    Add compatibility functions folder

---
+++ includes/functions/functions.compatibility.php
@@ -0,0 +1,9 @@
+<?php
+
+//compatibility
+if( !function_exists( 'str_contains' ) ) {
+	
+	function str_contains($haystack, $needle) {
+		return $needle !== '' && mb_strpos($haystack, $needle) !== false;
+	}
+}
